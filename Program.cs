94784fbae82e5c7f685942285eb8c2c14c1a8d60
Christa Stephens <christastephens@quickenloans.com>
2019-11-20 10:37:51 -0500

    added additional comments about how program.cs works with startus.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+//app starts which loads the .net core run time then the main method executes from
+//Program.cs which is the entry point for the application
+//It then kicks off create default builder which  is then called to makes use of our
+//start-up class
 namespace GardensApi
 {
     public class Program
