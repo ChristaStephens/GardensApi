030e6e5da8cdfb409f69a37a21f28ea5937194f7
Christa Stephens <christastephens@quickenloans.com>
2019-11-20 13:51:57 -0500

    comments

--- Controllers/GardensController.cs
+++ Controllers/GardensController.cs
@@ -2,6 +2,13 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 //namespace based on folder structure (root then folder)
+    //inherits from controller base
+    //not static
+    //to show that it is actually a controller we need to decorate
+    //it with "apicontroller"
+    //Route is the path for where the action method is being called.
+    //the action method is telling the browser what to return
+    //in this case return the action of iterating through the array.
 namespace GardensApi.Controllers
 {
     [Route("api/[controller]")]
