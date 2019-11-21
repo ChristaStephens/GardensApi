63352869c88d84b2aaad0a99577a2f6fb5ddf1a4
Christa Stephens <christastephens@quickenloans.com>
2019-11-21 10:24:38 -0500

    added sql connections

--- Models/GardenContext.cs
+++ Models/GardenContext.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 namespace GardensApi.Models
 {
     //inherits from dbcontext
+    //our db context need to be aware of our appsettingsjson
     public class GardenContext : DbContext
     {
         public GardenContext(DbContextOptions<GardenContext> options)
@@ -12,7 +13,7 @@ namespace GardensApi.Models
         {
 
         }
-        //this will be a table in our database
+        //this will be a table in our database that willl connect to our database
         public DbSet<Garden> GardenItems {get; set;}
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Configuration;
 
 //registers our services and started by create builder method from program.cs
 //startup calls two services: 1. configure services method - this is used throughout your app
@@ -16,6 +16,13 @@ namespace GardensApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         // In .Net 3 you gotta modify "AddMvc" in the Startup.cs to this:
         //services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+        public IConfiguration Configuration {get;}
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
